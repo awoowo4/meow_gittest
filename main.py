4b82a303987333ad840c4fb5be222c55205c94d3
миксяр <matveevv951@gmail.com>
2025-03-15 10:50:31 +0300

    добавлена функция для вычисления среднего значения

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-# sad
-print(123)
+def calculate_average(numbers):
+    return sum(numbers)/len(numbers)
+
+if __name__ == '__main__':
+    numbers = [1,2,3,4,5]
+    print('Среднее значение: ', calculate_average(numbers))
