c26a7fe0609356a4ae7e80f228726f8109a219a8
миксяр <matveevv951@gmail.com>
2025-03-15 11:16:08 +0300

    Revert "добавлена функция для вычисления среднего значения"
    This reverts commit 4b82a303987333ad840c4fb5be222c55205c94d3.

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-def calculate_average(numbers):
-    return sum(numbers)/len(numbers)
-
-if __name__ == '__main__':
-    numbers = [1,2,3,4,5]
-    print('Среднее значение: ', calculate_average(numbers))
+# sad
+print(123)
